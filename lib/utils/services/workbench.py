7979ba363656f18d3776381307aba3489a959db9
Zipho Mashologu <zipho.m@gmail.com>
2022-09-27 21:41:00 +0200

    restructuring and making modular

--- lib/utils/services/workbench.py
+++
@@ -1,7 +0,0 @@
-from lib.utils.services.factory import ServiceFactory
-from lib.utils.services.galaxy import GalaxyServiceBuilder
-from lib.utils.services.irida import IridaServiceBuilder
-
-factory = ServiceFactory()
-factory.register_builder("IRIDA", IridaServiceBuilder())
-factory.register_builder("GALAXY", GalaxyServiceBuilder())
